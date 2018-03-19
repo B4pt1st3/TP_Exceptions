2457ae1f9789e187d5997e6c97965d3a8c0ac3fe
B4pt1st3 <baptiste.moutinho@gmail.com>
2018-03-19 16:10:03 +0100

    Ajout d'une exception.

--- src/com/bmoutinho/Class1.java
+++ src/com/bmoutinho/Class1.java
@@ -1,10 +1,17 @@
 package com.bmoutinho;
 
+import java.io.File;
+
 public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		}
+		
 	}
 
 }
